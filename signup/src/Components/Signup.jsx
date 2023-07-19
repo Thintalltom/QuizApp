f877b8c72ebc53cff4eb7448727f58c4de5a8622
tomide <tomideadeyanju@gmail.com>
2023-07-19 19:34:51 +0100

    changes made

--- signup/src/Components/Signup.jsx
+++ signup/src/Components/Signup.jsx
@@ -1,57 +1,73 @@
-import React from 'react'
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { userContext } from '../Context/userContext'
-import { toast } from 'react-toastify'
+import React from "react";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { userContext } from "../Context/userContext";
+import { toast } from "react-toastify";
 const Signup = () => {
-  const { signUp, info} = useContext(userContext)
+  const { signUp, error } = useContext(userContext);
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [error, setError] = useState({})
-   const navigate = useNavigate()
-   const handleSignup = async (e) => {
+
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
     e.preventDefault();
-    setError('')
-    try {
-      // Validate the form here
-      const isValid = validateForm();
-      if (isValid) {
-        await signUp(email, password);
-        navigate('/login');
-        toast.success('You have successfully signed up');
-      } else {
-        // Set an error message here if the form is not valid
-        setError('Please fill all required fields');
-      }
-    } catch (err) {
-      console.log('An error occurred:', err);
-      setError(err.message || 'An unknown error occurred');
-      toast.error(error);
-    }
+    await signUp(email, password)
+      .then(() => {
+        //if error is equal to all this params display error
+        if (error == "auth/invalid-email" || "auth/email-already-in-use") {
+          toast.error(error);
+        } else if (!error) {
+          //if no error navigate to login
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        return err;
+      });
   };
 
-
- 
   return (
-    <div className='w-[100vw] h-[100vh] flex-col flex  items-center justify-center bg-slate-500'>
-      
-      <p className='text-4xl font-extrabold mb-4'>QUIZ APP</p>
-      <p className='text-slate-200 mb-4 font-bold'>Register Account</p>
-    <div className='text-slate-200 flex  items-center justify-center'>
-      
-        <form className='flex flex-col gap-9' >
-            <label>Gmail</label>
-            <input onChange={((e) => setEmail(e.target.value))} value={email} type='text' placeholder="input here " className=' text-slate-900 border-black-200 border-2 p-[5px] rounded w-[50vw] '  />
-            <label>Password</label>
-            <input onChange={((e) => setPassword(e.target.value))}  value={password} type='text' placeholder="input here " className=' text-slate-900  border-slate-200 border-2 w-[50vw] p-[5px] rounded' />
-        <button  onClick={handleSignup} className=' text-white bg-slate-900 rounded p-[10px]'>Sign up</button>
-        <p className='text-center'>already registered?  <Link to='/login' className='text-blue-900 cursor-pointer font-bold'>Log in</Link></p>
+    <div className="w-[100vw] h-[100vh] flex-col flex  items-center justify-center bg-slate-500">
+      <p className="text-4xl font-extrabold mb-4">QUIZ APP</p>
+      <p className="text-slate-200 mb-4 font-bold">Register Account</p>
+      <div className="text-slate-200 flex  items-center justify-center">
+        <form className="flex flex-col gap-9" onSubmit={handleSignup}>
+          <label>Gmail</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            placeholder="input here "
+            className=' xs:w-[80vw] sm:w-[50vw] text-slate-900 border-black border-2 p-[5px] rounded '
+          />
+          <label>Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="text"
+            placeholder="input here "
+            className=' xs:w-[80vw] sm:w-[50vw] text-slate-900  border-black border-2 p-[5px] rounded'
+          />
+          <button
+            type="submit"
+            className=' text-white bg-slate-900 rounded sm:w-[50vw] xs:w-[80vw] p-[10px]'
+          >
+            Sign up
+          </button>
+          <p className="text-center">
+            already registered?{" "}
+            <Link
+              to="/login"
+              className="text-blue-900 cursor-pointer font-bold"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
-        
-    
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
--- signup/src/Context/userContext.jsx
+++ signup/src/Context/userContext.jsx
@@ -1,60 +1,65 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { auth } from "./Firebase";
 import {
-  
   signInWithEmailAndPassword,
   signOut,
   createUserWithEmailAndPassword,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
 
 import { questions } from "../Components/QuizQuestions";
 export const userContext = createContext();
 
-
 export const UserProvider = (props) => {
   // Initialize Firebase Authentication and get a reference to the service
- 
+
   const [score, setScore] = useState(0);
   const [user, setUser] = useState({});
-  const [current, setCurrent] = useState((0));
-  
+  const [current, setCurrent] = useState(0);
+  const [error, setError] = useState({});
 
-  function signUp  (email, password) {
-    createUserWithEmailAndPassword(auth, email, password);
+  const signUp = async (email, password) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      //must catch the error here else wont be able to catch it at the main page
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
-  function login (email, password) {
+  function login(email, password) {
     signInWithEmailAndPassword(auth, email, password);
-  };
+  }
 
   function logOut() {
-   return signOut(auth)
+    return signOut(auth);
   }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser)
-      
-     });
-      return () => {
-        unsubscribe();
-      }   
-    }, []);
-
-  
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <userContext.Provider
       value={{
+        error,
         login,
         user,
         signUp,
-        logOut, score, setScore, current,setCurrent, questions
+        logOut,
+        score,
+        setScore,
+        current,
+        setCurrent,
+        questions,
       }}
     >
       {props.children}{" "}
     </userContext.Provider>
   );
 };
-
